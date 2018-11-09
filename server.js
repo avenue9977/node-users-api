5c4ce6599008fd490079ab2bfa3036f3881d26c2
Nikola Chetelyazov <nikolachetelyazov@Nikolas-MacBook-Pro.local>
2018-11-09 16:42:36 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,50 @@
+// set up 
+const express = require('express');
+const app = express();
+const path = require('path');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// API files for interacting with fake database
+const api = require('./routes/api');
+
+// Configuration 
+app.use(morgan('dev')); // log every request to the console
+
+// Parsers
+app.use(bodyParser.urlencoded({
+    'extended': 'true'
+}));
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS");
+    next();
+});
+
+app.use(bodyParser.json()); // parse application/json
+
+app.use(bodyParser.json({
+    type: 'application/vnd.api+json'
+}));
+
+app.use(methodOverride());
+
+// API url 
+app.use('/api', api);
+
+
+// Send all other requests to 404
+app.get('*', function(req, res) {
+    res.status(404);
+    res.send();
+});
+
+// Set port and listen (start app)
+var port = process.env.PORT || 8080;
+
+app.listen(port, function() {
+    console.log(`Running on localhost:${port}`);
+});
